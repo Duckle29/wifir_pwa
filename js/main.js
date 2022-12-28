b1e404c33f113b5b78b7de8c54aee2be7e9ccffb
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2022-12-28 04:02:48 +0100

    Added persistent settings via idb-keyval

--- js/main.js
+++ js/main.js
@@ -1,186 +1,213 @@
+let settings = {
+	'device_id': '', // 53d341
+	'mqtt_host': '', // wss://io.adafruit.com:443
+	'mqtt_user': '', // Duckle
+	'mqtt_pass': '', // aio_xxxxxxxxxxxxx
+};
+
 let sub_feeds = [
-    'temp',
-    'humi',
-    'eco2',
-    'tvoc',
-    'log',
-    'ir-state'
+	'temp',
+	'humi',
+	'eco2',
+	'tvoc',
+	'log',
+	'ir-state'
 ]
 
-let settings = {
-    'device_id': '53d341',
-    'mqtt_host': 'wss://io.adafruit.com:443',
-    'mqtt_user': 'Duckle',
-    'mqtt_pass': 'aio_Emph30wxehnRbQjDs8cAJc16TFXX'
-}
-
 let mqtt_options = {
-    // Clean session
-    'clean': true,
-    'connectTimeout': 4000,
+	// Clean session
+	'clean': true,
+	'connectTimeout': 4000,
 
-    // Auth
-    // clientId
-    username: settings['mqtt_user'],
-    password: settings['mqtt_pass']
+	// Auth
+	// clientId
+	username: settings['mqtt_user'],
+	password: settings['mqtt_pass']
 }
-
-let connection_state = false
 
 let sensor_values = {
-    'temp': 0,
-    'humi': 0,
-    'eco2': 0,
-    'tvoc': 0
+	'temp': 0,
+	'humi': 0,
+	'eco2': 0,
+	'tvoc': 0
 }
 
 
-function alert_banner(message, type, timeout=5000)
-{
-    let type_class = ''
+function alert_banner(message, type, timeout = 5000) {
+	let type_class = ''
 
-    switch(type){
-        case 'success':
-            type_class = 'alert-success'
-            svg = 'svg_success'
-            break;
-        
-        case 'info':
-            type_class = 'alert-primary'
-            svg = 'svg_info'
-            break;
-        
-        case 'warn':
-            type_class = 'alert-warning'
-            svg = 'svg_triangle'
-            break;
-        
-        case 'fail':
-            type_class = 'alert-danger'
-            svg = 'svg_triangle'
-            break;
-    }
+	switch (type) {
+		case 'success':
+			type_class = 'alert-success'
+			svg = 'svg_success'
+			break;
 
-    let $div_template = $(`<div  class='alert ${type_class} d-flex align-items-center visually-hidden' role='alert'>
-            <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success'><use xlink:href='#${svg}'/></svg>
-            <div>${message}</div>
-        </div>`)
+		case 'info':
+			type_class = 'alert-primary'
+			svg = 'svg_info'
+			break;
 
-    $div_template.click(function(){
-        $(this).fadeOut('fast', function(){
-            $(this).remove();
-        })
-    })
+		case 'warn':
+			type_class = 'alert-warning'
+			svg = 'svg_triangle'
+			break;
 
-    
-    $($div_template).fadeOut(10);
-    $($div_template).removeClass('visually-hidden');
-    $($div_template).fadeIn('slow');
+		case 'fail':
+			type_class = 'alert-danger'
+			svg = 'svg_triangle'
+			break;
+	}
 
-    setTimeout(function(){
-        $div_template.fadeOut('slow', function(){
-            $div_template.remove();
-        })
-    }, 5000)
+	let $div_template = $(`<div  class='alert ${type_class} d-flex align-items-center visually-hidden' role='alert'>
+						<svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success'><use xlink:href='#${svg}'/></svg>
+						<div>${message}</div>
+				</div>`)
 
-    $('#top_alerts').prepend($div_template)
+	$div_template.click(function() {
+		$(this).fadeOut('fast', function() {
+			$(this).remove();
+		})
+	})
+
+
+	$($div_template).fadeOut(10);
+	$($div_template).removeClass('visually-hidden');
+	$($div_template).fadeIn('slow');
+
+	setTimeout(function() {
+		$div_template.fadeOut('slow', function() {
+			$div_template.remove();
+		})
+	}, 5000)
+
+	$('#top_alerts').prepend($div_template)
 }
 
-$(document).ready(function(){
-    $('#settings_save').click(function(){
-        settings['device_id'] = $('#mqttDevice').val()
-        settings['mqtt_host'] = $('#mqttHost').val()
-        settings['mqtt_user'] = $('#mqttUser').val()
-        settings['mqtt_pass'] = $('#mqttPass').val()
-        console.log(settings)
 
-        $('#top_alert').text('Settings saved')
-        $('#top_alert').fadeOut(10)
-        $('#top_alert').removeClass('visually-hidden')
-        $('#top_alert').fadeIn('slow')
+$(document).ready(function() {
 
-        setTimeout(function(){
-            $('#top_alert').fadeOut('1000', function(){
-                $('#top_alert').addClass('visually-hidden')
-                $('#top_alert').text('')
-            })
-            
-        }, 5000)
-    })
+	for (let key in settings) {
+		idbKeyval.get(key).then((value) => {
+			settings[key] = value;
+			$(`#${key}`).val(value);
+		})
+	}
+
+	$('#settings_save').click(function() {
+		settings['device_id'] = $('#device_id').val()
+		settings['mqtt_host'] = $('#mqtt_host').val()
+		settings['mqtt_user'] = $('#mqtt_user').val()
+		settings['mqtt_pass'] = $('#mqtt_pass').val()
+		console.log(settings)
+
+		let tmp = []
+		for (let key in settings) {
+			tmp.push([key, settings[key]])
+		}
+
+		idbKeyval.setMany(tmp)
+			.then(() => console.log('It worked!'))
+			.catch((err) => console.log('It failed!', err));
+
+		$('#top_alert').text('Settings saved')
+		$('#top_alert').fadeOut(10)
+		$('#top_alert').removeClass('visually-hidden')
+		$('#top_alert').fadeIn('slow')
+
+		setTimeout(function() {
+			$('#top_alert').fadeOut('1000', function() {
+				$('#top_alert').addClass('visually-hidden')
+				$('#top_alert').text('')
+			})
+
+		}, 5000)
+	})
 })
 
 
 let client = null
 
-$('#connect_btn').click(function(){
-    if(client === null){
-        client = mqtt.connect(settings['mqtt_host'], mqtt_options)
-    }else{
-        client.reconnect()
-    }
-    
+$('#connect_btn').click(function() {
+	mqtt_options['username'] = settings['mqtt_user']
+	mqtt_options['password'] = settings['mqtt_pass']
 
-    client.on('message', function (topic, payload, packet) {
-        // Payload is Buffer
-        console.log(`Topic: ${topic}, Message: ${payload.toString()}, QoS: ${packet.qos}`)
-        
-        Object.entries(sensor_values).forEach((key, value) => {
-            if(topic.endsWith(key[0])){
-                sensor_values[key[0]] = parseFloat(payload.toString());
-            }
-        })
+	if (client === null) {
+		client = mqtt.connect(settings['mqtt_host'], mqtt_options)
+	} else {
+		client.reconnect()
+	}
 
-        $('#temp_out').text(sensor_values['temp'])
-        $('#humi_out').text(sensor_values['humi'])
 
-    })
+	client.on('message', function(topic, payload, packet) {
+		// Payload is Buffer
+		console.log(`Topic: ${topic}, Message: ${payload.toString()}, QoS: ${packet.qos}`)
 
-    client.on('close', function(){
-        connection_state = false
-    
-        $('#connect_btn').fadeIn('fast')
-        $('#disconnect_btn').fadeOut('fast')
-    })
+		Object.entries(sensor_values).forEach((key, value) => {
+			if (topic.endsWith(key[0])) {
+				sensor_values[key[0]] = parseFloat(payload.toString());
+			}
+		})
 
-    client.on('reconnect', function(){
-        connection_state = true
-    
-        $('#connect_btn').fadeOut('fast')
-        $('#disconnect_btn').fadeIn('fast')
-    })
-    
-    client.on('connect', function(){
-        console.log('Connected to MQTT')
-        connection_state = true
-        
-        $('#connect_btn').fadeOut('fast')
-        $('#disconnect_btn').fadeIn('fast')
+		$('#temp_out').text(sensor_values['temp'])
+		$('#humi_out').text(sensor_values['humi'])
 
-        alert_banner('Connected', 'success')
-    
-        let feeds = ''
-        feeds = sub_feeds
-        for(let i=0; i<feeds.length; i++) {
-            feeds[i] = `${settings['mqtt_user']}/feeds/wifir-${settings['device_id']}-${feeds[i]}`
-        }
-        
-        client.subscribe(feeds, { qos: 0 }, function (error, granted) {
-            if (error) {
-                console.log(error)
-            } else {
-                granted.forEach(element => {
-                    console.log(`${element.topic} was subscribed`)
-                });
-            }
-        })    
-    
-        client.publish(`${settings['mqtt_user']}/feeds/wifir-${settings['device_id']}-config`, 'get_sensors', { qos: 0, retain: false }, function (error) {
-            if (error) {
-                console.log(error)
-            }
-            else {
-                console.log('requested_sensors')
-            }
-        })
-    })
+	})
+
+	client.on('close', function() {
+
+		$('#connect_btn').fadeIn('fast')
+		$('#disconnect_btn').fadeOut('fast')
+	})
+
+	client.on('reconnect', function() {
+
+		$('#connect_btn').fadeOut('fast')
+		$('#disconnect_btn').fadeIn('fast')
+	})
+
+	client.on('connect', function() {
+		console.log('Connected to MQTT')
+
+		$('#connect_btn').fadeOut('fast')
+		$('#disconnect_btn').fadeIn('fast')
+
+		alert_banner('Connected', 'success')
+
+		let feeds = ''
+		feeds = sub_feeds
+		for (let i = 0; i < feeds.length; i++) {
+			feeds[i] = `${settings['mqtt_user']}/feeds/wifir-${settings['device_id']}-${feeds[i]}`
+		}
+
+		client.subscribe(feeds, {
+			qos: 0
+		}, function(error, granted) {
+			if (error) {
+				console.log(error)
+			} else {
+				granted.forEach(element => {
+					console.log(`${element.topic} was subscribed`)
+				});
+			}
+		})
+
+		client.publish(`${settings['mqtt_user']}/feeds/wifir-${settings['device_id']}-config`, 'get_sensors', {
+			qos: 0,
+			retain: false
+		}, function(error) {
+			if (error) {
+				console.log(error)
+			} else {
+				console.log('requested_sensors')
+			}
+		})
+	})
+})
+
+$('#disconnect_btn').click(function(){
+	if (client === null) {
+		console.log('shrug')
+	} else {
+		client.end()
+	}
 })
