baef64315b152c240d0e6270e38140e8b850b64e
Mikkel Jeppesen <6906805-Duckle29@users.noreply.gitlab.com>
2023-06-28 17:03:07 +0200

    Added update functionality to service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-let cacheName = 'hello-pwa';
+let version = '2.0.0'
+let cacheName = 'v' + version + '_wifir-pwa';
 let filesToCache = [
   '/',
   '/index.html',
@@ -20,5 +21,34 @@ self.addEventListener('fetch', function(e) {
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
+  );
+});
+
+/* Update PWA on version mismatch */
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.filter(function(cacheName) {
+                  if (!cacheName.startsWith(cacheName)) {
+                      return true;
+                  }
+              }).map(function(cacheName) {
+                  // completely deregister for ios to get changes too
+                  console.log('deregistering Serviceworker')
+                  if ('serviceWorker' in navigator) {
+                      navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                          registrations.map(r => {
+                              r.unregister()
+                          })
+                      })
+                      window.location.reload(true)
+                  }
+
+                  console.log('Removing old cache.', cacheName);
+                  return caches.delete(cacheName);
+              })
+          );
+      })
   );
 });
